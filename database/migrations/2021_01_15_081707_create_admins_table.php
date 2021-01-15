d8833b67cd1412063dbf6c1dce0d0f815f2964e3
Nguyen Thi Tham <thamnt@hblab.vn>
2021-01-15 17:43:43 +0700

    refactor migration create admins table

--- database/migrations/2021_01_15_081707_create_admins_table.php
+++ database/migrations/2021_01_15_081707_create_admins_table.php
@@ -15,11 +15,11 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 255);
+            $table->string('email', 255);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone_number')->nullable();
+            $table->string('password', 25);
+            $table->string('phone_number', 25)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
