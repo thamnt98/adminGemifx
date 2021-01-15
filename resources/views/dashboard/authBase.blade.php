b61cb842a3d24afaea89798d9624cf11529ae61f
Nguyen Thi Tham <thamnt@hblab.vn>
2021-01-15 17:28:22 +0700

    create view login and register

---
+++ resources/views/dashboard/authBase.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+* CoreUI Free Laravel Bootstrap Admin Template
+* @version v2.0.1
+* @link https://coreui.io
+* Copyright (c) 2020 creativeLabs Łukasz Holeczek
+* Licensed under MIT (https://coreui.io/license)
+-->
+
+<html lang="en">
+  <head>
+    <base href="./">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="author" content="Łukasz Holeczek">
+    <title>GemiFx</title>
+    <meta name="theme-color" content="#ffffff">
+    <!-- Icons-->
+    <link href="{{ asset('css/free.min.css') }}" rel="stylesheet"> <!-- icons -->
+    <!-- Main styles for this application-->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics-->
+    <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      // Shared ID
+      gtag('config', 'UA-118965717-3');
+      // Bootstrap ID
+      gtag('config', 'UA-118965717-5');
+    </script>
+
+    <link href="{{ asset('css/coreui-chartjs.css') }}" rel="stylesheet">
+
+  </head>
+  <body class="c-app flex-row align-items-center">
+
+    @yield('content') 
+
+    <!-- CoreUI and necessary plugins-->
+    <script src="{{ asset('js/coreui.bundle.min.js') }}"></script>
+
+    @yield('javascript')
+
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/register', 'Auth\RegisterController@main')->name('register');
+Route::get('/login', 'Auth\LoginController@main')->name('login');
