50c30209988afa53dab2c1016f18bc7920aaebe6
Nguyen Thi Tham <thamnt@hblab.vn>
2021-01-28 14:48:46 +0700

    refactor code

--- app/Http/Controllers/Admin/User/DetailController.php
+++ app/Http/Controllers/Admin/User/DetailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\User;
 use App\Http\Controllers\Controller;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DetailController extends Controller
 {
@@ -25,7 +26,7 @@ class DetailController extends Controller
     public function main($id)
     {
         $user = $this->userRepository->find($id);
-        $isAdmin = $user->role == config('role.admin');
+        $isAdmin = Auth::user()->role == config('role.admin');
         return view('admin.user.detail', compact('user', 'isAdmin'));
     }
 }
