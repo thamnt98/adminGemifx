4b92e0bed620801da67dc335b816cfa4883425b8
thamnt98 <tham27011@gmail.com>
2021-02-17 15:31:35 +0700

    add loader

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -138,3 +138,4 @@ class UserRepository extends EloquentBaseRepository implements RepositoryInterfa
         ]);
     }
 }
+//904453
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,7 +8,7 @@
 -->
 
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
@@ -17,28 +17,33 @@
     <!-- Main styles for this application-->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    @yield('css')
-    <!-- Global site tag (gtag.js) - Google Analytics-->
+@yield('css')
+<!-- Global site tag (gtag.js) - Google Analytics-->
     <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
-  </head>
-  <body class="c-app">
-    <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
+</head>
+<body class="c-app">
+<div id="overlay">
+    <div id="progstat"></div>
+    <div id="progress"></div>
+</div>
+<div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
 
-      @include('layouts.sidebar')
+    @include('layouts.sidebar')
 
-      @include('layouts.header')
+    @include('layouts.header')
 
-      <div class="c-body">
+    <div class="c-body">
 
         <main class="c-main">
 
-          @yield('content')
+            @yield('content')
 
         </main>
         @include('layouts.footer')
-      </div>
     </div>
-    <script src="{{ asset('js/coreui.bundle.min.js') }}"></script>
-    @yield('javascript')
-  </body>
+</div>
+<script src="{{ asset('js/coreui.bundle.min.js') }}"></script>
+<script src="{{ asset('js/loader.js') }}"></script>
+@yield('javascript')
+</body>
 </html>
