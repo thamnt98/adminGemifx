43458f644034c9baf9fd101fceac8a417964d350
thamnt98 <tham27011@gmail.com>
2021-02-19 14:53:38 +0700

    change favicon and title page

--- resources/views/dashboard/authBase.blade.php
+++ resources/views/dashboard/authBase.blade.php
@@ -8,42 +8,44 @@
 -->
 
 <html lang="en">
-  <head>
+<head>
     <base href="./">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="author" content="Łukasz Holeczek">
-    <title>GemiFx</title>
+    <title> GemiFX -  Forex Brokers | FX Trading Software | While Label</title>
     <meta name="theme-color" content="#ffffff">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <!-- Global site tag (gtag.js) - Google Analytics-->
     <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
     <script>
-      window.dataLayer = window.dataLayer || [];
+        window.dataLayer = window.dataLayer || [];
 
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-      // Shared ID
-      gtag('config', 'UA-118965717-3');
-      // Bootstrap ID
-      gtag('config', 'UA-118965717-5');
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        // Shared ID
+        gtag('config', 'UA-118965717-3');
+        // Bootstrap ID
+        gtag('config', 'UA-118965717-5');
     </script>
 
     <link href="{{ asset('css/coreui-chartjs.css') }}" rel="stylesheet">
 
-  </head>
-  <body class="c-app flex-row align-items-center">
+</head>
+<body class="c-app flex-row align-items-center">
 
-    @yield('content') 
+@yield('content')
 
-    <!-- CoreUI and necessary plugins-->
-    <script src="{{ asset('js/coreui.bundle.min.js') }}"></script>
+<!-- CoreUI and necessary plugins-->
+<script src="{{ asset('js/coreui.bundle.min.js') }}"></script>
 
-    @yield('javascript')
+@yield('javascript')
 
-  </body>
+</body>
 </html>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,10 +12,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Gemifx</title>
+    <title> GemiFX -  Forex Brokers | FX Trading Software | While Label</title>
     <!-- Icons-->
     <!-- Main styles for this application-->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 @yield('css')
 <!-- Global site tag (gtag.js) - Google Analytics-->
