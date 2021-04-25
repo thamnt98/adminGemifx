5a80320e02aea27484d3dda9fbe093c080e3ec4b
thamnt98 <tham27011@gmail.com>
2021-04-25 13:36:32 +0700

    add online chart

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -45,6 +45,19 @@
 </div>
 <script src="{{ asset('js/coreui.bundle.min.js') }}"></script>
 <script src="{{ asset('js/loader.js') }}"></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/5e9410fa35bcbb0c9ab06470/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 @yield('javascript')
 </body>
 </html>
